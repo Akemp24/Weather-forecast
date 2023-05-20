e0038ffa36c11d6310a07d70dca31007cef45cd9
Asiak <asiakemp24@gmail.com>
2023-05-20 17:41:37 -0400

    started on function for saving the local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
-var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+// make a function so that you can recall previous searches (make in get ready state)
+$(function () {
+    // list all global variables here
+    // pull from local storage
+    var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
+    // make function for saving the search based on the local storage
+    function saveSearch () {
+        var search = $("search-city").val
+
+        if (search !== "") {
+            searchHistory.push(search);
+            localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+            $("#search-city").val("");
+           
+            displaySearchHistory()
+        }
+    }
+})
