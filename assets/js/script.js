9bbce277a83438a835040aeb0ea08a17583ccd47
Asiak <asiakemp24@gmail.com>
2023-05-20 21:34:26 -0400

    got the save city history functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,40 @@
-// make a function so that you can recall previous searches (make in get ready state)
-$(function () {
+$(document).ready(function() {
     // list all global variables here
     // pull from local storage
     var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
-
+  
     // make function for saving the search based on the local storage
-    function saveSearch () {
-        var search = $("search-city").val
+    function saveSearch() {
+      var search = $('#search-city').val();
+  
+      if (search !== '') {
+        searchHistory.push(search);
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+        $('#search-city').val('');
 
-        if (search !== "") {
-            searchHistory.push(search);
-            localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-            $("#search-city").val("");
-           
-            showSearchHistory()
-        }
+          showSearchHistory();
+      }
+    }
+  
+    function showSearchHistory() {
+      var showHistory = $('#search-history');
+
+      showHistory.empty();
+  
+      $.each(searchHistory, function(index, value) {
+        var searchItem = $('<p>').text(value);
+        showHistory.append(searchItem);
+      });
     }
 
-    function showSearchHistory () {
-        var showHistory = $("#search-history");
-        showHistory.empty();
-        $.each(searchHistory, function(index, value) {
-            var searchItem = $("<p>").text(value);
-            showHistory.append(searchItem);
-        });
+    // add click event so it'll happen on button click
+    // later add function here so that weather event will happen
+    function buttonClick() {
+      saveSearch();
     }
-
-
-
-
-
-})
+  
+    // Add event listener for the search button click
+    $('#search-btn').on('click', buttonClick);
+  
+    showSearchHistory();
+  });
